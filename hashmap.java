5998757c1ddedaf2fa32e6f6f81afab060eaacde
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-13 15:53:17 +0530

    Add files via upload

---
+++ hashmap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hashmap
+{
+  public static void main(String args[])
+  {
+    HashMap<String,String> a=new HashMap<>();
+    a.put("sno","01");
+    a.put("sname","muraja");
+    System.out.println(a);
+  }
+}
